e5efb948d2050a859958f2496727e9e4e31cde21
MASTAN VALI DUDEKULA <129518068+validudekula@users.noreply.github.com>
2023-05-03 20:42:10 +0530

    Update script.js
    commit changes

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data analyst trainee"],
+        strings: ["Data science trainee"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data analyst trainee"],
+        strings: ["Data science trainee"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
